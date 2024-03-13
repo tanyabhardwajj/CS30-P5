cf94e78cbdddd4e381f33bf6adb09dc1aac840c3
tanyabhardwajj <160178263+tanyabhardwajj@users.noreply.github.com>
2024-03-13 15:13:46 -0600

    13 March additions

--- Perlin Noice assignment/sketch.js
+++ Perlin Noice assignment/sketch.js
@@ -8,17 +8,24 @@ let rectWidth = 2;
 let rectHeight;
 
 function setup() {
+  document.addEventListener("contextmenu", event => event.preventDefault())
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
 }
 
 function terrain(){
   let time = 0;
-  let x = 0;
+  let x = 0;    
+  let tallest = 0;
+  let xTallest = 0;
   while(x < width){
     rectHeight = noise(time);
     rectHeight = map(rectHeight, 0, 1, 0, height*0.90);
+    if(rectHeight > tallest){
+      tallest = rectHeight;
+      }
     time += 0.005;
+    fill(0);
     rect(x, height , x + rectWidth, height - rectHeight);
     x += rectWidth;
   }
@@ -27,11 +34,19 @@ function terrain(){
 // do a rectWidth if statement for letting the program not crash
 function keyPressed(){
   if(keyCode === RIGHT_ARROW){
+    if(rectWidth <= 3.5){
     rectWidth = rectWidth + 0.5;
+    }
   }
   else if(keyCode === LEFT_ARROW){
+    if(rectWidth >= 1){
     rectWidth = rectWidth - 0.5;
+    }
   }
+}
+
+function highestPoint(){
+
 }
 
 function draw() {
