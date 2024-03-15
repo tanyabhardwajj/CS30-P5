dfbaf7c4620a0a37599d4cdfb6af514ba2dd0533
tanyabhardwajj <7003657@spsd.sk.ca>
2024-03-14 19:44:17 -0600

    at home additions
    Doesn't work for some reason. Ask mr. Scott for help.

--- Grid Assignment/sketch.js
+++ Grid Assignment/sketch.js
@@ -8,12 +8,15 @@
 
 
 //Global Variable
-let gridSize = 83.11; // Initial size of the squares
+let gridSize = 20; // Initial size of the squares
+let cols = width / gridSize;
+let rows = height / gridSize;
 
 
 function setup() {
   document.addEventListener("contextmenu", event => event.preventDefault())
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  background(255);
   grid();
 }
 
@@ -21,11 +24,13 @@ function setup() {
 //function for drawing the grid : draws the grid and fills with
 //random shades of pink
 function grid() {
-  for (let x = 0; x < width; x += gridSize) {//horizontal lines of grid
-    for (let y = 0; y < height; y += gridSize) {//vertical lines of grid
+  for (let x = 0; x < cols; x ++) {//horizontal lines of grid
+    for (let y = 0; y < rows; y ++) {//vertical lines of grid
+      if ((x * gridSize < width && y * gridSize < height)){  
       fill(random(200, 255), random(150, 200), random(150, 200)); // Shades of pink
       //using rect to easily fill the squares of the grid
-      rect(x, y, gridSize, gridSize);
+      rect(x * gridSize, y * gridSize, gridSize, gridSize);
+      }
     }
   }
 }
@@ -36,20 +41,21 @@ function mousePressed() {
   if (mouseButton === RIGHT) {
     //makes the program stop at the largest gridSize without 
     //having the squares fall off
-    if (gridSize < 166) {
+    //if (gridSize < 166) {
       gridSize = gridSize / 0.5; // to increase the gridSize
       grid();
     }
-  }
-  else if (mouseButton === LEFT) {
+    else if (mouseButton === LEFT) {
     //makes the program stop at the smallest gridSize without
     //having the squares fall off
-    if (gridSize > 41.555) {
+    //if (gridSize > 41.555) {
       gridSize = gridSize * 0.5; // to decrease the gridSize
       grid();
     }
   }
-}
+  
+  //}
+//}
 
 //function for refreshing colors of the grid
 function keyPressed() {
