9c46d75c51e89536b7016f2bef83fb7d818c549e
tanyabhardwajj <7003657@spsd.sk.ca>
2024-04-16 20:42:07 -0600

    16 April at home additions

--- Assignment Cars Cars Cars/sketch.js
+++ Assignment Cars Cars Cars/sketch.js
@@ -3,20 +3,41 @@
 // 12 April, 2024
 // 
 
+//Global Variables
+let eastbound = [];
+let westbound = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i < 20; i++){
+    let laneY = random(height/3, 2*height/3);
+    let car = new Vehicle(random(width), laneY, random([0,1]), color(random(255), random(255), random(255)), random(2) > 1 ? 1 : -1);
+    if(car.direction === 1){
+      eastbound.push(car);
+    }
+    else{
+      westbound.push(car);
+    }
+  }
 }
 
 function draw() {
   background(220);
   drawRoad();
-  truck();
+  for (let i = 0; i < eastbound.length; i++) {
+    eastbound[i].action();
+  }
+  
+  for (let i = 0; i < westbound.length; i++) {
+    westbound[i].action();
+  }
 }
 
 function drawRoad(){
   rectMode = CENTER;
   fill(0);
-  rect(0, height*0.25, width*2, 500)
+  rect(0, height*0.25, width*2, 300)
+  //white dashes
   for(let i = 0; i < width; i+=40){
     stroke(220);
     strokeWeight(5);
@@ -27,11 +48,11 @@ function drawRoad(){
 
 
 class Vehicle{
-  constructor(x, y){
+  constructor(x, y, type, color, direction){
     this.x = x;
     this. y = y;
     this.type = type;
-    this.c = color(random(255), random(255), random(255));
+    this.c = color;
     this.direction = direction;
     this.xSpeed = random(1, 5)*this.direction
   }
@@ -44,6 +65,7 @@ class Vehicle{
     else if(this.type === 1){
       rect(this.x, this.c, 70, 40)
     }
+
   }
 
   move(){
@@ -58,7 +80,7 @@ class Vehicle{
 
   speedUp(){
     if(this.xSpeed < 15 && this.xSpeed > -15){
-      this.xSpeed += this.dorection*0.5;
+      this.xSpeed += this.direction*0.5;
     }
   }
 
@@ -66,5 +88,23 @@ class Vehicle{
     if(this.xSpeed > 0.5 || this.xSpeed < -0.5){
       this.xSpeed -= this.direction*0.5;
     }
+  }
+
+  changeColor(){
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  action(){
+    this.move();
+    if(random(100) < 1){
+      this.speedUp();
+    }
+    if(random(100) < 1){
+      this.speedDown();
+    }
+    if (random(100) < 1){
+      this.changeColor();
+    }
+    this.display();
   }
 }
