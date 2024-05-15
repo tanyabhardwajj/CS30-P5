6806bddf4f3b97097a1aee151f86b000d360ef55
tanyabhardwajj <160178263+tanyabhardwajj@users.noreply.github.com>
2024-05-15 15:19:57 -0600

    15 May 2024 additions
    Pissing off Mr. Scott

--- Perlin Noice assignment/sketch.js
+++ Perlin Noice assignment/sketch.js
@@ -7,10 +7,8 @@
 let rectWidth = 2;
 let rectHeight;
 let randomHeight;
-let highestY = 0;
-let highestX = 0;
-let x = 0;
 let scrollSpeed = 0;
+//for average line
 let totalrectHeight = 0;
 let rectNumber = 0;
 let averageHeight = 0;
@@ -20,18 +18,27 @@ function setup() {
   rectMode(CORNERS);
 }
 
-let time = 0;
 function terrain(){
+  let time = 0;
+  let highestY = 0;
+  let highestX = 0;
+  let x = 0;
+  totalrectHeight = 0;
+  rectNumber = 0;
   while(x < width){
+    //panning occurs because of scrollspeed
     scrollSpeed += 0.0002;
     randomHeight = noise(time+scrollSpeed);
     randomHeight = map(randomHeight, 0, 1, 0, height * 0.8);
     time += 0.03;
-    fill(0);
+    fill(255, 105, 180);//hot pink color to annoy Mr. Scott
     noStroke();
     rectHeight = height - randomHeight;
     rect(x, height, x + rectWidth, rectHeight);
     x += rectWidth;
+
+
+    //to determine highest peaks(including the peak's x and y value)
     if(randomHeight > highestY){
       highestY = randomHeight;
       highestX = x - rectWidth;
@@ -39,6 +46,8 @@ function terrain(){
     if(rectWidth > highestX){
       highestX = x;
     }
+
+    //
     totalrectHeight += rectHeight;
     rectNumber += 1;
   }
@@ -48,14 +57,14 @@ function terrain(){
 function drawFlag(x,y){
   let longSquare = 50;
   let square = 20;
-  fill(0);
+  fill(139,0, 0);
   rect(x, y - longSquare, x + 5, y);
   rect(x, y - longSquare - square, x + square, y - longSquare);
 }
 
 function averageLine(){
   averageHeight = totalrectHeight / rectNumber;
-  stroke(255, 0, 0);
+  stroke(255, 0, 0);//Dark red to annoy Mr. Scott
   strokeWeight(5);
   rect(0, averageHeight, width, averageHeight);
 }
